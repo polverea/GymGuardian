9afae23536848826b9bbb95f2b6f00f84372f324
polverea <polvereamarius@yahoo.com>
2024-05-20 23:28:07 +0300

    Version 2 working menu

--- app/src/main/java/com/example/gymguardian/MainActivity.kt
+++ app/src/main/java/com/example/gymguardian/MainActivity.kt
@@ -5,41 +5,47 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.gymguardian.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
-
+        bottomNavigationView = binding.bottomNavigationView
         binding.SignOutButton.setOnClickListener {
             Firebase.auth.signOut()
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
 
-        binding.bottomNavigationView.setOnItemReselectedListener {
-
-            when(it.itemId) {
-                R.id.home -> replaceFragment(Home())
-                R.id.profile -> replaceFragment(Profile())
-                R.id.food -> replaceFragment(Food())
-
-                else -> {
-
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    true
                 }
-            }
-                true
+
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                R.id.food -> {
+                    replaceFragment(FoodFragment())
+                    true
+                }
+                else -> false
             }
         }
+        replaceFragment(HomeFragment())
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
     }
-
-
+}
