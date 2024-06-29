179593ca6c9d216375136545e284822c0e85d2d6
polverea <polvereamarius@yahoo.com>
2024-06-29 15:06:31 +0300

    V7: Big update in food fragment, adding meal sections and connect to open food facts api for food search etc

--- app/src/main/java/com/example/gymguardian/FoodFragment.kt
+++ app/src/main/java/com/example/gymguardian/FoodFragment.kt
@@ -1,9 +1,14 @@
 package com.example.gymguardian
 
+import SearchResultsAdapter
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -11,11 +16,16 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.gymguardian.api.RetrofitInstance
 import com.example.gymguardian.databinding.FragmentFoodBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -47,7 +57,8 @@ class FoodFragment : Fragment() {
 
     private fun setupDateNavigation() {
         updateDateTextView()
-        binding.root.findViewById<View>(R.id.previousDayButton).setOnClickListener { changeDate(-1) }
+        binding.root.findViewById<View>(R.id.previousDayButton)
+            .setOnClickListener { changeDate(-1) }
         binding.root.findViewById<View>(R.id.nextDayButton).setOnClickListener { changeDate(1) }
     }
 
@@ -78,18 +89,22 @@ class FoodFragment : Fragment() {
     }
 
     private fun setupAddMealButtons() {
-        binding.root.findViewById<View>(R.id.breakfastSection).findViewById<View>(R.id.addMealButton).setOnClickListener {
-            showAddMealDialog("breakfast")
-        }
-        binding.root.findViewById<View>(R.id.lunchSection).findViewById<View>(R.id.addMealButton).setOnClickListener {
-            showAddMealDialog("lunch")
-        }
-        binding.root.findViewById<View>(R.id.dinnerSection).findViewById<View>(R.id.addMealButton).setOnClickListener {
-            showAddMealDialog("dinner")
-        }
-        binding.root.findViewById<View>(R.id.snacksSection).findViewById<View>(R.id.addMealButton).setOnClickListener {
-            showAddMealDialog("snacks")
-        }
+        binding.root.findViewById<View>(R.id.breakfastSection)
+            .findViewById<View>(R.id.addMealButton).setOnClickListener {
+                showSearchMealDialog("breakfast")
+            }
+        binding.root.findViewById<View>(R.id.lunchSection).findViewById<View>(R.id.addMealButton)
+            .setOnClickListener {
+                showSearchMealDialog("lunch")
+            }
+        binding.root.findViewById<View>(R.id.dinnerSection).findViewById<View>(R.id.addMealButton)
+            .setOnClickListener {
+                showSearchMealDialog("dinner")
+            }
+        binding.root.findViewById<View>(R.id.snacksSection).findViewById<View>(R.id.addMealButton)
+            .setOnClickListener {
+                showSearchMealDialog("snacks")
+            }
     }
 
     private fun loadMeals() {
@@ -126,60 +141,137 @@ class FoodFragment : Fragment() {
                     totalFat += meal.fat
                 }
 
-                sectionView.findViewById<TextView>(R.id.totalCaloriesTextView).text = "Calories: $totalCalories kcal"
-                sectionView.findViewById<TextView>(R.id.totalMacrosTextView).text = "Carbs: ${totalCarbs}g, Protein: ${totalProtein}g, Fat: ${totalFat}g"
+                sectionView.findViewById<TextView>(R.id.totalCaloriesTextView).text =
+                    "Calories: $totalCalories kcal"
+                sectionView.findViewById<TextView>(R.id.totalMacrosTextView).text =
+                    "Carbs: ${totalCarbs}g, Protein: ${totalProtein}g, Fat: ${totalFat}g"
 
                 val recyclerView = sectionView.findViewById<RecyclerView>(R.id.recyclerView)
                 recyclerView.adapter = MealAdapter(mealList)
             }
             .addOnFailureListener { e ->
-                Toast.makeText(context, "Failed to load $mealType: ${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    context,
+                    "Failed to load $mealType: ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
     }
 
-    private fun showAddMealDialog(mealType: String) {
+    private fun showSearchMealDialog(mealType: String) {
         val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_add_meal, null)
         val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogView)
         val alertDialog = dialogBuilder.show()
 
-        val mealNameEditText = dialogView.findViewById<EditText>(R.id.mealNameEditText)
-        val caloriesEditText = dialogView.findViewById<EditText>(R.id.caloriesEditText)
-        val carbsEditText = dialogView.findViewById<EditText>(R.id.carbsEditText)
-        val proteinEditText = dialogView.findViewById<EditText>(R.id.proteinEditText)
-        val fatEditText = dialogView.findViewById<EditText>(R.id.fatEditText)
-        val quantityEditText = dialogView.findViewById<EditText>(R.id.quantityEditText)
+        val mealNameEditText = dialogView.findViewById<AutoCompleteTextView>(R.id.mealNameEditText)
+        val searchResultsRecyclerView = dialogView.findViewById<RecyclerView>(R.id.searchResultsRecyclerView)
+        searchResultsRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        dialogView.findViewById<View>(R.id.saveMealButton).setOnClickListener {
-            val mealName = mealNameEditText.text.toString().trim()
-            val calories = caloriesEditText.text.toString().trim().toIntOrNull()
-            val carbs = carbsEditText.text.toString().trim().toIntOrNull()
-            val protein = proteinEditText.text.toString().trim().toIntOrNull()
-            val fat = fatEditText.text.toString().trim().toIntOrNull()
-            val quantity = quantityEditText.text.toString().trim().toIntOrNull()
-
-            if (mealName.isEmpty() || calories == null || carbs == null || protein == null || fat == null || quantity == null) {
-                Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+        mealNameEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.isNotEmpty()) {
+                    searchFood(s.toString()) { foodItems ->
+                        if (foodItems != null && foodItems.isNotEmpty()) {
+                            val adapter = SearchResultsAdapter(foodItems) { selectedFoodItem ->
+                                showFoodDetailsDialog(selectedFoodItem, mealType)
+                                alertDialog.dismiss()
+                            }
+                            searchResultsRecyclerView.adapter = adapter
+                        } else {
+                            Toast.makeText(context, "Food not found", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
             }
 
-            val user = auth.currentUser
-            user?.let {
-                val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                val today = sdf.format(selectedDate.time)
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-                val meal = Meal(mealName, calories, carbs, protein, fat, quantity)
-                db.collection("UsersInfo").document(it.uid)
-                    .collection("Meals").document(today)
-                    .collection(mealType)
-                    .add(meal)
-                    .addOnSuccessListener {
-                        Toast.makeText(context, "Meal added successfully", Toast.LENGTH_SHORT).show()
-                        alertDialog.dismiss()
-                        loadMeals()
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    private fun showFoodDetailsDialog(foodItem: Product, mealType: String) {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_food_details, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogView)
+        val alertDialog = dialogBuilder.show()
+
+        val foodNameTextView = dialogView.findViewById<TextView>(R.id.foodNameTextView)
+        val caloriesTextView = dialogView.findViewById<TextView>(R.id.caloriesTextView)
+        val carbsTextView = dialogView.findViewById<TextView>(R.id.carbsTextView)
+        val proteinTextView = dialogView.findViewById<TextView>(R.id.proteinTextView)
+        val fatTextView = dialogView.findViewById<TextView>(R.id.fatTextView)
+        val quantityEditText = dialogView.findViewById<EditText>(R.id.quantityEditText)
+        val addMealButton = dialogView.findViewById<Button>(R.id.addMealButton)
+
+        foodNameTextView.text = foodItem.product_name
+        caloriesTextView.text = "Calories: ${(foodItem.nutriments.energyKcal ?: 0f).toInt()} kcal"
+        carbsTextView.text = "Carbs: ${(foodItem.nutriments.carbohydrates ?: 0f).toInt()}g"
+        proteinTextView.text = "Protein: ${(foodItem.nutriments.proteins ?: 0f).toInt()}g"
+        fatTextView.text = "Fat: ${(foodItem.nutriments.fat ?: 0f).toInt()}g"
+
+        addMealButton.setOnClickListener {
+            val quantity = quantityEditText.text.toString().toIntOrNull() ?: 100
+            val factor = quantity / 100.0
+
+            val calories = (foodItem.nutriments.energyKcal ?: 0f) * factor
+            val carbs = (foodItem.nutriments.carbohydrates ?: 0f) * factor
+            val protein = (foodItem.nutriments.proteins ?: 0f) * factor
+            val fat = (foodItem.nutriments.fat ?: 0f) * factor
+
+            addSelectedFoodToMeal(foodItem, mealType, alertDialog, calories.toInt(), carbs.toInt(), protein.toInt(), fat.toInt(), quantity)
+        }
+    }
+
+    private fun addSelectedFoodToMeal(
+        selectedFoodItem: Product,
+        mealType: String,
+        alertDialog: AlertDialog,
+        calories: Int,
+        carbs: Int,
+        protein: Int,
+        fat: Int,
+        quantity: Int
+    ) {
+        val user = auth.currentUser
+        user?.let {
+            val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val today = sdf.format(selectedDate.time)
+
+            val meal = Meal(selectedFoodItem.product_name, calories, carbs, protein, fat, quantity)
+            db.collection("UsersInfo").document(it.uid)
+                .collection("Meals").document(today)
+                .collection(mealType)
+                .add(meal)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Meal added successfully", Toast.LENGTH_SHORT).show()
+                    alertDialog.dismiss()
+                    loadMeals()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(context, "Failed to add meal: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun searchFood(foodName: String, callback: (List<Product>?) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = RetrofitInstance.api.searchProducts(foodName)
+                if (response.isSuccessful && response.body() != null) {
+                    val products = response.body()?.products ?: emptyList()
+                    withContext(Dispatchers.Main) {
+                        callback(products)
                     }
-                    .addOnFailureListener { e ->
-                        Toast.makeText(context, "Failed to add meal: ${e.message}", Toast.LENGTH_SHORT).show()
+                } else {
+                    withContext(Dispatchers.Main) {
+                        callback(null)
                     }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                withContext(Dispatchers.Main) {
+                    callback(null)
+                }
             }
         }
     }
